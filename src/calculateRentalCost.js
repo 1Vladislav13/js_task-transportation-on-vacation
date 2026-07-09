63fba5b6d08589db87f622ba1bddadd59754d8b2
Uladzislau <patricijaholman@gmail.com>
2026-07-09 10:23:07 +0200

    fix solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,11 +5,15 @@
  */
 function calculateRentalCost(days) {
   let result = days * 40;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const shortTerm = 3;
+  const shortTermDiscount = 20;
 
-  if (days >= 7) {
-    result -= 50;
-  } else if (days >= 3) {
-    result -= 20;
+  if (days >= longTerm) {
+    return (result -= longTermDiscount);
+  } else if (days >= shortTerm) {
+    return (result -= shortTermDiscount);
   }
 
   return result;
